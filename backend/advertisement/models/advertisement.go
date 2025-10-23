adc04ccc87bdc47822dba71ddc2b4c016accd537
airandember <air@airandember.com>
2025-10-23 04:03:02 -0600

    database cleanup

--- backend/advertisement/models/advertisement.go
+++ backend/advertisement/models/advertisement.go
@@ -174,7 +174,7 @@ type AdAuditLog struct {
 // Note: Migration constants are now defined in database.go for unified management
 
 // SeedAdPlacements inserts default ad placements
-func (db *database.DB) SeedAdPlacements() error {
+func SeedAdPlacements(db *database.DB) error {
 	placements := []struct {
 		Name        string
 		Description string
